f4b3ba6c0d1e3b7d5f768673ba29e5f941a979d5
mrblueh <53908900+mrblueh@users.noreply.github.com>
2023-10-31 02:14:09 +0800

    Update 测试.js

--- 测试.js
+++ 测试.js
@@ -1,4 +1,4 @@
 var body = $response.body;
-body = body.replace(/"duration":3,"begin_time":1698422400,"end_time":1698508799/g, '"duration":0,"begin_time":0,"end_time":0,');
+body = body.replace(/"duration":\d,"begin_time":\d+,"end_time":\d+,/g, '"duration":0,"begin_time":0,"end_time":0,');
 $done({body});
 
