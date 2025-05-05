7afd6ba46ef974a96e234041890bf9b3ce94a50d
mrblueh <53908900+mrblueh@users.noreply.github.com>
2025-05-06 06:57:45 +0800

    更新 photomator.js

--- photomator.js
+++ photomator.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/Photomator.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/Photomator.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/mrblueh/Quantumult-X/refs/heads/master/photomator.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/mrblueh/Quantumult-X/refs/heads/master/photomator.js
 
 [mitm]
 hostname = api.revenuecat.com
